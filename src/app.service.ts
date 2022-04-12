d30e7d54ee3713bb7b2d8f904d843893d2fe418d
RicholeYu <richole.yu@qq.com>
2022-04-12 18:19:07 +0800

    增加提示

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Richole!';
+    return 'Hello Docer Nestjs!';
   }
 }
